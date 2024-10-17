7d8c31faab0c566117a0870001cb90765c140133
Sawon-52 <mehedihasansawon.52@gmail.com>
2024-10-17 20:31:44 +0600

    Redirecting new records to the edit page

--- src/routes/EditContact.jsx
+++ src/routes/EditContact.jsx
@@ -14,7 +14,7 @@ const EditContact = () => {
     <Form method="post" id="contact-form">
       <p>
         <span>Name</span>
-        <input placeholder="First" aria-label="First name" type="text" name="first" defaultValue={contact?.first} />
+        <input placeholder="First" aria-label="First name" type="text" name="first"  defaultValue={contact?.first} />
         <input placeholder="Last" aria-label="Last name" type="text" name="last" defaultValue={contact?.last} />
       </p>
       <label>
